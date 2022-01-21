06fecd461799a411d38bcd8a5f6781e8207ad21e
dendersen <dendersen@endersen.dk>
2022-01-21 09:02:36 +0100

    calc fib numbers without errors

--- fibenaci.js
+++ fibenaci.js
@@ -11,22 +11,22 @@ function wait(ms) {
 }
 
 async function calc(){
-  // try{
+  try{
     if(fib.length<=90){//close to accurate up to 80, then looses accuracy until 101 where it stops calculating
       fib [fib.length] = parseInt(parseInt(fib[fib.length-1]) + parseInt(fib[fib.length-2]))
       console.log(fib.length-1, fib[fib.length-1])
       wait (1)
       calc()
-      return
+      return(fib[fib.length-1])
     }
     console.log(fib.length + " numbers of fibonaci sequence calculated")
     console.log("scroll up to see them numbered")
     console.log(fib)
-// }catch(err){
-//     console.log(fib.length + " numbers of fibonaci sequence calculated")
-//     console.log("scroll up to see them numbered")
-//     console.log(fib)
-//   }
+}catch(err){
+    console.log(fib.length + " numbers of fibonaci sequence calculated")
+    console.log("scroll up to see them numbered")
+    console.log(fib)
+  }
 }
 
 calc()
